771a6d0cc36d5f6fa145f4441d4cd85e119f39e0
sebasrosalesr <7sebasrr@gmail.com>
2025-11-14 10:33:16 -0800

    Update __init__.py

--- skybar/intents/__init__.py
+++ skybar/intents/__init__.py
@@ -1,3 +1,4 @@
 from .ticket_status import intent_ticket_status
 from .customer_tickets import intent_customer_tickets
 from .credit_numbers import intent_rtn_summary
+from .record_lookup import intent_record_lookup
